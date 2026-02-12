3666a1786da45e0ad6f8efa2d46b03b5cfb7ba64
Marcos <marcosaurelioce@hotmail.com>
2026-02-11 22:13:34 -0300

    chore: initial commit with Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">
+        Tailwind funcionando 🚀
+      </h1>
+    </div>
+  );
+}
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// filepath: tailwind.config.js
+module.exports = {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#3B82F6",
+				success: "#10B981",
+				warning: "#F59E0B",
+				danger: "#EF4444",
+				gray: "#6B7280",
+			},
+		},
+	},
+	plugins: [],
+};
